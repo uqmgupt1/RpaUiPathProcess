85fb64918ff2a56e5ceaeabf0d2ff060b38e539f
Steve Debbe <s.debbe@uq.edu.au>
2020-03-23 14:03:32 +1000

    Code cleanup for publishing

--- Processes/HR_FixedTermAppointmentExpiry/Archive/Data/script - Backup.js
+++
@@ -1,7 +0,0 @@
-function(element,input){
-var xhr = new XMLHttpRequest();
-xhr.open("POST","yourUrl",true);
-xhr.setRequestHeader('Content-Type','application/json');
-xhr.send(JSON.stringify(jsonText));
-return xhr.status + ""
-}
